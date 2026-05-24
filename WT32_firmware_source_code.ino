fec76a802ce1fe30b66fe3e8f547f1517f8ea0f8
mohamed <168596078+slimanbe@users.noreply.github.com>
2026-05-24 18:03:08 +0100

    Update WT32_firmware_source_code.ino

--- WT32_firmware_source_code.ino
+++ WT32_firmware_source_code.ino
@@ -1,5 +1,5 @@
 #include <WiFi.h>
-#include <WebServer.h>
+#include <HTTPClient.h>
 #include <PZEM004Tv30.h>
 #include <ArduinoJson.h>
 
@@ -8,58 +8,33 @@ const char* ssid = "realme C51";
 const char* password = "40042026";
 
 
-PZEM004Tv30 pzem(Serial2, 16, 17); 
+const char* serverUrl = "http://your-server-ip:5000/data";  
 
-WebServer server(80);
+const String deviceID = "node01";   
 
-String getSensorData() {
-  StaticJsonDocument<200> doc;
-  
-  float v = pzem.voltage();
-  float c = pzem.current();
-  float p = pzem.power();
-  float e = pzem.energy();
-  float f = pzem.frequency();
-  float pf = pzem.pf();
+PZEM004Tv30 pzem(Serial2, 16, 17);  
 
-  doc["voltage"] = isnan(v) ? 0 : v;
-  doc["current"] = isnan(c) ? 0 : c;
-  doc["power"] = isnan(p) ? 0 : p;
-  doc["energy"] = isnan(e) ? 0 : e;
-  doc["frequency"] = isnan(f) ? 0 : f;
-  doc["pf"] = isnan(pf) ? 0 : pf;
 
-  String json;
-  serializeJson(doc, json);
-  return json;
-}
 
 void setup() {
-  
   Serial.begin(9600);
   
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) { 
-    delay(500); 
-    Serial.print("."); 
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  
   Serial.println("\nConnected! IP Address: " + WiFi.localIP().toString());
-
-  server.on("/data", []() {
-    server.send(200, "application/json", getSensorData());
-  });
-
-  server.begin();
 }
 
 void loop() {
-  server.handleClient();
+  static unsigned long lastSend = 0;
 
-  static unsigned long lastPrint = 0;
-  if (millis() - lastPrint > 2000) {
-    lastPrint = millis();
+  if (millis() - lastSend > 2000) {        
+    lastSend = millis();
 
+    // Read data from PZEM
     float v = pzem.voltage();
     float c = pzem.current();
     float p = pzem.power();
@@ -67,19 +42,54 @@ void loop() {
     float f = pzem.frequency();
     float pf = pzem.pf();
 
-    Serial.println("\n--- PZEM-004T Measurements ---");
-    if (isnan(v)) {
-      Serial.println("Error reading from PZEM module!");
-    } else {
-      Serial.print("Voltage: "); Serial.print(v); Serial.println(" V");
-      Serial.print("Current: "); Serial.print(c); Serial.println(" A");
-      Serial.print("Power:   "); Serial.print(p); Serial.println(" W");
-      Serial.print("Energy:  "); Serial.print(e); Serial.println(" kWh");
-      Serial.print("Freq:    "); Serial.print(f); Serial.println(" Hz");
-      Serial.print("PF:      "); Serial.println(pf);
+    
+    if (isnan(v) || isnan(c) || isnan(p)) {
+      Serial.println("Error reading from PZEM - Skipping this reading");
+      return;  
     }
+
+    // Create JSON payload
+    StaticJsonDocument<300> doc;
+    doc["device_id"] = deviceID;
+    doc["voltage"] = v;
+    doc["current"] = c;
+    doc["power"] = p;
+    doc["energy"] = e;
+    doc["frequency"] = f;
+    doc["pf"] = pf;
+    doc["timestamp"] = millis();
+
+    String jsonPayload;
+    serializeJson(doc, jsonPayload);
+
+    // Send via HTTP POST
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      http.begin(serverUrl);
+      http.addHeader("Content-Type", "application/json");
+
+      int httpResponseCode = http.POST(jsonPayload);
+
+      if (httpResponseCode > 0) {
+        Serial.printf("Data sent successfully! Response: %d\n", httpResponseCode);
+      } else {
+        Serial.printf("Failed to send data. Error: %s\n", http.errorToString(httpResponseCode).c_str());
+      }
+      
+      http.end();
+    } else {
+      Serial.println("WiFi disconnected. Reconnecting...");
+      WiFi.reconnect();
+    }
+
+    
+    Serial.println("\n--- PZEM-004T Measurement Sent ---");
+    Serial.print("Voltage: "); Serial.print(v); Serial.println(" V");
+    Serial.print("Current: "); Serial.print(c); Serial.println(" A");
+    Serial.print("Power: "); Serial.print(p); Serial.println(" W");
+    Serial.print("Energy: "); Serial.print(e); Serial.println(" kWh");
     Serial.println("--------------------------------");
   }
-  
-  delay(10); 
+
+  delay(10);
 }
